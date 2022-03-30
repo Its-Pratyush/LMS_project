edfde021b501d29c69f300ed8cb89ccc0fb384a0
clear44 <96740074+clear44@users.noreply.github.com>
2022-03-30 10:03:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch((err) => console.log(err));
 
 
-app.use(routes);
+
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -91,5 +91,7 @@ app.post('/login', async(req, res) => {
 })
 
 app.get('/profile', (req, res) => {
+    res.render('profile')
 
 })
+app.use(routes);
